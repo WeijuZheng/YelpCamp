3c57496c0e139f5be70e5c65e89db8d434b560ad
weiju <weijuzheng1995@gmail.com>
2020-12-01 15:16:56 -0800

    add start script

--- app.js
+++ app.js
@@ -159,6 +159,8 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('SERVING ON PORT 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`SERVING ON PORT ${port}`);
 });
